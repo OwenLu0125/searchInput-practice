a9ae23ce00c2bfb1f9638b63b03d7ccc67c17557
OwenLu <gg0125gg@gmail.com>
2024-01-01 16:56:40 +0800

    style: cancel dark theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,12 @@
 'use client'
 import * as React from 'react';
 import { Autocomplete, TextField, Stack } from '@mui/material'
-
-interface CityData {
-  city: string;
-  growth_from_2000_to_2013: string;
-  latitude: number;
-  longitude: number;
-  population: string;
-  rank: string;
-  state: string;
-}
+import { CityData } from '../types/citydata';
 
 export default function Home() {
   const [data, setData] = React.useState<CityData[]>([]);
 
-  React.useEffect(() =>{
+  React.useEffect(() => {
     const fetchData = async () => {
       try {
         const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
@@ -28,11 +19,11 @@ export default function Home() {
     };
 
     fetchData();
-  }, []); 
+  }, []);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Stack spacing={2} sx={{ width: 400 }}>
-        <Autocomplete  
+        <Autocomplete
           id="free-solo-demo"
           freeSolo
           options={data.map((option) => ({
---
+++ src/types/citydata.ts
@@ -0,0 +1,9 @@
+export type CityData = {
+  city: string;
+  growth_from_2000_to_2013: string;
+  latitude: number;
+  longitude: number;
+  population: string;
+  rank: string;
+  state: string;
+}
